406f34014a54817a64536d40f4c87b336f87e973
Kevin <kevin@dendron.so>
2023-09-10 15:29:52 -0700

    adding labels

--- instrumentation.js
+++ instrumentation.js
@@ -6,8 +6,19 @@ const {
 const {
   OTLPLogsExporter
 } = require('@opentelemetry/exporter-logs-otlp-proto');
+const { Resource } = require('@opentelemetry/resources');
+const {
+  SemanticResourceAttributes,
+} = require('@opentelemetry/semantic-conventions');
 
+const resource = Resource.default().merge(
+  new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: "otel-node-demo",
+    [SemanticResourceAttributes.SERVICE_VERSION]: "0.1.0",
+  }),
+);
 const sdk = new NodeSDK({
+  resource,
   logRecordProcessor: new SimpleLogRecordProcessor(new OTLPLogsExporter()),
 });
 sdk.start();
