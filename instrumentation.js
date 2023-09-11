e56a61e1ff511ca6f95c51154a86f9283b664c3f
Kevin <kevin@dendron.so>
2023-09-10 21:15:01 -0700

    export the trace

--- instrumentation.js
+++ instrumentation.js
@@ -13,16 +13,20 @@ const {
 const {
   getNodeAutoInstrumentations,
 } = require('@opentelemetry/auto-instrumentations-node');
+const {
+  OTLPTraceExporter,
+} = require('@opentelemetry/exporter-trace-otlp-proto');
 
 const resource = Resource.default().merge(
   new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: "otel-node-demo",
-    [SemanticResourceAttributes.SERVICE_VERSION]: "0.1.1",
+    [SemanticResourceAttributes.SERVICE_VERSION]: "0.1.2",
   }),
 );
 const sdk = new NodeSDK({
   resource,
   logRecordProcessor: new SimpleLogRecordProcessor(new OTLPLogsExporter()),
+  traceExporter: new OTLPTraceExporter(),
   instrumentations: [getNodeAutoInstrumentations()],
 });
 sdk.start();
